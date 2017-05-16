258f1d22db249ac6fb26f5f4c131c4529309aa5f
Vitaliy Sokolov <sokolovvitalik@gmail.com>
2017-05-17 01:00:31 +0300

    update client

---
+++ shared/socket.io/events.js
@@ -0,0 +1,18 @@
+module.exports = {
+    CHAT_JOIN: 'join',
+    CHAT_LEAVE: 'leave',
+    DISCONNECT: 'disconnect',
+    ROOMS: 'rooms',
+    USERS: 'users',
+    ROOM_USERS: 'room users',
+    ADD_ROOM: 'add room',
+    DELETE_ROOM: 'delete room',
+    JOIN_ROOM: 'join room',
+    LEAVE_ROOM: 'leave room',
+    INVITE_USER: 'invite user',
+    MESSAGES: 'messages',
+    MESSAGE: 'message',
+    EDIT_MESSAGE: 'edit message',
+    DELETE_MESSAGE: 'delete message',
+    ERROR_MESSAGE: 'error message'
+};
